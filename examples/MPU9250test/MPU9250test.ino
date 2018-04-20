d90bcde1de49cd63e30282d2e7c272b403089284
simondlevy <simon.d.levy@gmail.com>
2018-04-19 23:31:29 -0400

    Cleanup

--- examples/MPU9250test/MPU9250test.ino
+++ examples/MPU9250test/MPU9250test.ino
@@ -2,6 +2,7 @@
 
 #include "MPU9250.h"
 #include "quaternionFilters.h"
+
 /*
  MPU9250 Configuration
 
@@ -9,23 +10,25 @@
 
  Choices are:
 
-  Gscale:   GFS_250 == 250 dps, 
-            GFS_500 DPS == 500 dps, 
-            GFS_1000 == 1000 dps, 
-            GFS_2000DPS == 2000 degrees per second gyro full scale
+  Gscale:   GFS_250 = 250 dps, GFS_500 = 500 dps, GFS_1000 = 1000 dps, GFS_2000DPS = 2000 degrees per second gyro full scale
 
-  Ascale: AFS_2G == 2 g, AFS_4G == 4 g, AFS_8G == 8 g, and AFS_16G == 16 g accelerometer full scale
+  Ascale: AFS_2G = 2 g, AFS_4G = 4 g, AFS_8G = 8 g, and AFS_16G = 16 g accelerometer full scale
 
-  Mscale: MFS_14BITS == 0.6 mG per LSB and MFS_16BITS == 0.15 mG per LSB
+  Mscale: MFS_14BITS = 0.6 mG per LSB and MFS_16BITS = 0.15 mG per LSB
 
-  Mmode: Mmode == M_8Hz for 8 Hz data rate or Mmode = M_100Hz for 100 Hz data rate
-  (1 + sampleRate) is a simple divisor of the fundamental 1000 kHz rate of the gyro and accel, so 
+  Mmode: Mmode = M_8Hz for 8 Hz data rate or Mmode = M_100Hz for 100 Hz data rate
 
+  sampleRate: (1 + sampleRate) is a simple divisor of the fundamental 1000 kHz rate of the gyro and accel, so 
   sampleRate = 0x00 means 1 kHz sample rate for both accel and gyro, 0x04 means 200 Hz, etc.
 */
+static const uint8_t Gscale     = GFS_250DPS;
+static const uint8_t Ascale     = AFS_2G;
+static const uint8_t Mscale     = MFS_16BITS;
+static const uint8_t Mmode      = M_100Hz;
+static const uint8_t sampleRate = 0x04;         
 
-static const uint8_t Gscale = GFS_250DPS, Ascale = AFS_2G, Mscale = MFS_16BITS, Mmode = M_100Hz, sampleRate = 0x04;         
-static float aRes, gRes, mRes;      // scale resolutions per LSB for the sensors
+// scale resolutions per LSB for the sensors
+static float aRes, gRes, mRes;
 
 // global constants for 9 DoF fusion and AHRS (Attitude and Heading Reference System)
 static const float pi = 3.141592653589793238462643383279502884f;
@@ -41,19 +44,21 @@ static const uint8_t ledPin = 13; // red led
 static MadgwickQuaternion quat(beta);
 
 // Interrupt support 
-static bool newData = false;
+static bool gotNewData = false;
 static void myinthandler()
 {
-    newData = true;
+    gotNewData = true;
 }
 
-static float   magCalibration[3]; // Factory mag calibration and mag bias
+// Factory mag calibration and mag bias
+static float   magCalibration[3]; 
 
 // Bias corrections for gyro and accelerometer. These can be measured once and
 // entered here or can be calculated each time the device is powered on.
 static float gyroBias[3], accelBias[3], magBias[3], magScale[3];      
 
-static MPU9250 imu; // instantiate MPU9250 class
+// Instantiate MPU9250 class
+static MPU9250 imu; 
 
 static uint8_t  yawBytes[2], pitchBytes[2], rollBytes[2]; // for writing to SPI flash
 
@@ -73,19 +78,24 @@ void setup()
     Wire.setClock(400000); // I2C frequency at 400 kHz
     delay(1000);
 
-    //imu.I2Cscan(); // should detect BME280 at 0x77, MPU9250 at 0x71 
+    imu.I2Cscan(); // should detect BME280 at 0x77, MPU9250 at 0x71 
 
     // Set up the interrupt pin, it's set as active high, push-pull
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, HIGH); // start with orange led on (active HIGH)
-
     pinMode(intPin, INPUT);
 
-    /* Configure the MPU9250 */
+    // Start with orange led on (active HIGH)
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, HIGH); 
+
+    // Configure the MPU9250 
     // Read the WHO_AM_I register, this is a good test of communication
     Serial.println("MPU9250 9-axis motion sensor...");
     uint8_t c = imu.getMPU9250ID();
-    Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX); Serial.print(" I should be "); Serial.println(0x71, HEX);
+    Serial.print("MPU9250 ");
+    Serial.print("I AM ");
+    Serial.print(c, HEX);
+    Serial.print(" I should be ");
+    Serial.println(0x71, HEX);
     delay(1000);
 
     if (c == 0x71 ) // WHO_AM_I should always be 0x71 for MPU9250, 0x73 for MPU9255 
@@ -94,15 +104,28 @@ void setup()
 
         imu.resetMPU9250(); // start by resetting MPU9250
 
-        float   SelfTest[6];    // holds results of gyro and accelerometer self test
+        float SelfTest[6];    // holds results of gyro and accelerometer self test
 
         imu.SelfTest(SelfTest); // Start by performing self test and reporting values
-        Serial.print("x-axis self test: acceleration trim within : "); Serial.print(SelfTest[0],1); Serial.println("% of factory value");
-        Serial.print("y-axis self test: acceleration trim within : "); Serial.print(SelfTest[1],1); Serial.println("% of factory value");
-        Serial.print("z-axis self test: acceleration trim within : "); Serial.print(SelfTest[2],1); Serial.println("% of factory value");
-        Serial.print("x-axis self test: gyration trim within : "); Serial.print(SelfTest[3],1); Serial.println("% of factory value");
-        Serial.print("y-axis self test: gyration trim within : "); Serial.print(SelfTest[4],1); Serial.println("% of factory value");
-        Serial.print("z-axis self test: gyration trim within : "); Serial.print(SelfTest[5],1); Serial.println("% of factory value");
+
+        Serial.print("x-axis self test: acceleration trim within : "); 
+        Serial.print(SelfTest[0],1); 
+        Serial.println("% of factory value");
+        Serial.print("y-axis self test: acceleration trim within : "); 
+        Serial.print(SelfTest[1],1); 
+        Serial.println("% of factory value");
+        Serial.print("z-axis self test: acceleration trim within : "); 
+        Serial.print(SelfTest[2],1); 
+        Serial.println("% of factory value");
+        Serial.print("x-axis self test: gyration trim within : "); 
+        Serial.print(SelfTest[3],1); 
+        Serial.println("% of factory value");
+        Serial.print("y-axis self test: gyration trim within : "); 
+        Serial.print(SelfTest[4],1); 
+        Serial.println("% of factory value");
+        Serial.print("z-axis self test: gyration trim within : "); 
+        Serial.print(SelfTest[5],1); 
+        Serial.println("% of factory value");
         delay(1000);
 
         // get sensor resolutions, only need to do this once
@@ -112,31 +135,51 @@ void setup()
 
         // Comment out if using pre-measured, pre-stored offset biases
         imu.calibrateMPU9250(gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
-        Serial.println("accel biases (mg)"); Serial.println(1000.*accelBias[0]); Serial.println(1000.*accelBias[1]); Serial.println(1000.*accelBias[2]);
-        Serial.println("gyro biases (dps)"); Serial.println(gyroBias[0]); Serial.println(gyroBias[1]); Serial.println(gyroBias[2]);
+        Serial.println("accel biases (mg)");
+        Serial.println(1000.*accelBias[0]);
+        Serial.println(1000.*accelBias[1]);
+        Serial.println(1000.*accelBias[2]);
+        Serial.println("gyro biases (dps)");
+        Serial.println(gyroBias[0]);
+        Serial.println(gyroBias[1]);
+        Serial.println(gyroBias[2]);
         delay(1000); 
 
         imu.initMPU9250(Ascale, Gscale, sampleRate); 
-        Serial.println("MPU9250 initialized for active data mode...."); // Initialize device for active mode read of acclerometer, gyroscope, and temperature
+        Serial.println("MPU9250 initialized for active data mode...."); 
 
         // Read the WHO_AM_I register of the magnetometer, this is a good test of communication
         byte d = imu.getAK8963CID();  // Read WHO_AM_I register for AK8963
-        Serial.print("AK8963 "); Serial.print("I AM "); Serial.print(d, HEX); Serial.print(" I should be "); Serial.println(0x48, HEX);
+        Serial.print("AK8963 ");
+        Serial.print("I AM ");
+        Serial.print(d, HEX);
+        Serial.print(" I should be ");
+        Serial.println(0x48, HEX);
         delay(1000); 
 
         // Get magnetometer calibration from AK8963 ROM
-        imu.initAK8963(Mscale, Mmode, magCalibration); Serial.println("AK8963 initialized for active data mode...."); // Initialize device for active mode read of magnetometer
+        imu.initAK8963(Mscale, Mmode, magCalibration);
+        Serial.println("AK8963 initialized for active data mode...."); 
 
         // Comment out if using pre-measured, pre-stored offset biases
         imu.magcalMPU9250(magBias, magScale);
-        Serial.println("AK8963 mag biases (mG)"); Serial.println(magBias[0]); Serial.println(magBias[1]); Serial.println(magBias[2]); 
-        Serial.println("AK8963 mag scale (mG)"); Serial.println(magScale[0]); Serial.println(magScale[1]); Serial.println(magScale[2]); 
+        Serial.println("AK8963 mag biases (mG)");
+        Serial.println(magBias[0]);
+        Serial.println(magBias[1]);
+        Serial.println(magBias[2]); 
+        Serial.println("AK8963 mag scale (mG)");
+        Serial.println(magScale[0]);
+        Serial.println(magScale[1]);
+        Serial.println(magScale[2]); 
         delay(2000); // add delay to see results before serial spew of data
 
         Serial.println("Calibration values: ");
-        Serial.print("X-Axis sensitivity adjustment value "); Serial.println(magCalibration[0], 2);
-        Serial.print("Y-Axis sensitivity adjustment value "); Serial.println(magCalibration[1], 2);
-        Serial.print("Z-Axis sensitivity adjustment value "); Serial.println(magCalibration[2], 2);
+        Serial.print("X-Axis sensitivity adjustment value ");
+        Serial.println(magCalibration[0], 2);
+        Serial.print("Y-Axis sensitivity adjustment value ");
+        Serial.println(magCalibration[1], 2);
+        Serial.print("Z-Axis sensitivity adjustment value ");
+        Serial.println(magCalibration[2], 2);
 
         attachInterrupt(intPin, myinthandler, RISING);  // define interrupt for intPin output of MPU9250
 
@@ -156,23 +199,22 @@ void loop()
     static int16_t MPU9250Data[7]; // used to read all 14 bytes at once from the MPU9250 accel/gyro
     static float sum;
     static uint32_t sumCount;         // used to control display output rate
-    static uint32_t lastUpdate;                 // used to calculate integration interval
+    static uint32_t timePrev;                 // used to calculate integration interval
     static float ax, ay, az, gx, gy, gz, mx, my, mz;
     static float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};    // vector to hold quaternion
 
 
     // If intPin goes high, either all data registers have new data
     // or the accel wake on motion threshold has been crossed
-    if(newData) {   // On interrupt, read data
+    if(gotNewData) {   // On interrupt, read data
 
-        newData = false;     // reset newData flag
-
+        gotNewData = false;     // reset gotNewData flag
 
         if(imu.checkNewAccelGyroData())  // data ready interrupt is detected
         {
             imu.readMPU9250Data(MPU9250Data); // INT cleared on any read
 
-            // Now we'll calculate the accleration value into actual g's
+            // timeCurr we'll calculate the accleration value into actual g's
             ax = (float)MPU9250Data[0]*aRes - accelBias[0];  // get actual g value, this depends on scale being set
             ay = (float)MPU9250Data[1]*aRes - accelBias[1];   
             az = (float)MPU9250Data[2]*aRes - accelBias[2];  
@@ -190,7 +232,8 @@ void loop()
 
                 // Calculate the magnetometer values in milliGauss
                 // Include factory calibration per data sheet and user environmental corrections
-                mx = (float)magCount[0]*mRes*magCalibration[0] - magBias[0];  // get actual magnetometer value, this depends on scale being set
+                // Get actual magnetometer value, this depends on scale being set
+                mx = (float)magCount[0]*mRes*magCalibration[0] - magBias[0];  
                 my = (float)magCount[1]*mRes*magCalibration[1] - magBias[1];  
                 mz = (float)magCount[2]*mRes*magCalibration[2] - magBias[2];  
                 mx *= magScale[0];
@@ -199,11 +242,13 @@ void loop()
             }
         }
 
-
         for(uint8_t i = 0; i < 5; i++) { // iterate a fixed number of times per data read cycle
-            uint32_t Now = micros();
-            float deltat = ((Now - lastUpdate)/1000000.0f); // set integration time by time elapsed since last filter update
-            lastUpdate = Now;
+
+            uint32_t timeCurr = micros();
+
+            // set integration time by time elapsed since last filter update
+            float deltat = ((timeCurr - timePrev)/1000000.0f); 
+            timePrev = timeCurr;
 
             sum += deltat; // sum for averaging filter update rate
             sumCount++;
@@ -211,32 +256,48 @@ void loop()
             quat.update(-ax, ay, az, gx*pi/180.0f, -gy*pi/180.0f, -gz*pi/180.0f, my, -mx, mz, deltat, q);
         }
 
-        /* end of MPU9250 interrupt handling */
-    }
-
+    } // if (gotNewData)
 
     // Serial print and/or display at 0.5 s rate independent of data rates
     if(sumCount > 500) {
 
-        Serial.print("ax = "); Serial.print((int)1000*ax);  
-        Serial.print(" ay = "); Serial.print((int)1000*ay); 
-        Serial.print(" az = "); Serial.print((int)1000*az); Serial.println(" mg");
-        Serial.print("gx = "); Serial.print( gx, 2); 
-        Serial.print(" gy = "); Serial.print( gy, 2); 
-        Serial.print(" gz = "); Serial.print( gz, 2); Serial.println(" deg/s");
-        Serial.print("mx = "); Serial.print( (int)mx ); 
-        Serial.print(" my = "); Serial.print( (int)my ); 
-        Serial.print(" mz = "); Serial.print( (int)mz ); Serial.println(" mG");
+        Serial.print("ax = ");
+        Serial.print((int)1000*ax);  
+        Serial.print(" ay = ");
+        Serial.print((int)1000*ay); 
+        Serial.print(" az = ");
+        Serial.print((int)1000*az);
+        Serial.println(" mg");
+        Serial.print("gx = ");
+        Serial.print( gx, 2); 
+        Serial.print(" gy = ");
+        Serial.print( gy, 2); 
+        Serial.print(" gz = ");
+        Serial.print( gz, 2);
+        Serial.println(" deg/s");
+        Serial.print("mx = ");
+        Serial.print( (int)mx ); 
+        Serial.print(" my = ");
+        Serial.print( (int)my ); 
+        Serial.print(" mz = ");
+        Serial.print( (int)mz );
+        Serial.println(" mG");
 
-        Serial.print("q0 = "); Serial.print(q[0]);
-        Serial.print(" qx = "); Serial.print(q[1]); 
-        Serial.print(" qy = "); Serial.print(q[2]); 
-        Serial.print(" qz = "); Serial.println(q[3]); 
+        Serial.print("q0 = ");
+        Serial.print(q[0]);
+        Serial.print(" qx = ");
+        Serial.print(q[1]); 
+        Serial.print(" qy = ");
+        Serial.print(q[2]); 
+        Serial.print(" qz = ");
+        Serial.println(q[3]); 
 
         float temperature = ((float) MPU9250Data[3]) / 333.87f + 21.0f; // Gyro chip temperature in degrees Centigrade
 
         // Print temperature in degrees Centigrade      
-        Serial.print("Gyro temperature is ");  Serial.print(temperature, 1);  Serial.println(" degrees C"); // Print T values to tenths of s degree C
+        Serial.print("Gyro temperature is ");  
+        Serial.print(temperature, 1);  
+        Serial.println(" degrees C"); 
 
         float a12 =   2.0f * (q[1] * q[2] + q[0] * q[3]);
         float a22 =   q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3];
@@ -278,7 +339,9 @@ void loop()
         Serial.print(", ");
         Serial.print(lin_az*1000.0f, 2);  Serial.println(" mg");
 
-        Serial.print("rate = "); Serial.print((float)sumCount/sum, 2); Serial.println(" Hz");
+        Serial.print("rate = ");
+        Serial.print((float)sumCount/sum, 2);
+        Serial.println(" Hz\n");
 
         sumCount = 0;
         sum = 0;    
