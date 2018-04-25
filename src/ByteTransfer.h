9930669ea1f5799633deb2b48d71b6ed0ff932e3
Dr. Simon Levy <levy@python.cs.wlu.edu>
2018-04-25 15:40:35 -0400

    Begin abstract ByteTransfer interface

---
+++ src/ByteTransfer.h
@@ -0,0 +1,17 @@
+/*  Header file for abstract ByteTransfer class supporting I^2C and SPI
+ *
+ *  Copyright 2018 Simon D. Levy
+ *  
+ *  Library may be used freely and without limit with attribution.
+ */
+
+#pragma once
+
+class ByteTransfer
+{
+    protected: 
+
+        virtual void    writeByte(uint8_t address, uint8_t subAddress, uint8_t data) = 0;
+        virtual uint8_t readByte(uint8_t address, uint8_t subAddress) = 0;
+        virtual void    readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest) = 0;
+};
