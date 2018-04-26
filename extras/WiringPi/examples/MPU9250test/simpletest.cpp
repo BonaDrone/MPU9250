3d75f19a09a4ea25928167b99469a226ba9df02d
simondlevy <simon.d.levy@gmail.com>
2018-04-26 21:17:56 +0000

    More testing

--- extras/WiringPi/examples/MPU9250test/simpletest.cpp
+++ extras/WiringPi/examples/MPU9250test/simpletest.cpp
@@ -14,17 +14,19 @@ static const uint32_t SPI_SPEED = 400000;
 
 int main(int argc, char ** argv)
 {
+    // Begin SPI
+    if (wiringPiSPISetup(SPI_BUS, SPI_SPEED) < 0) {
+        fprintf(stderr, "Unable to start SPI\n");
+        exit(1);
+    }
+
     // Set up SPI transfer object
     WiringPiSPI bt = WiringPiSPI(SPI_BUS, SPI_SPEED);
 
     // Instantiate MPU9250 class
     MPU9250 imu = MPU9250(&bt); 
 
-    // Begin SPI
-    if (wiringPiSPISetup(SPI_BUS, SPI_SPEED) < 0) {
-        fprintf(stderr, "Unable to start SPI\n");
-        exit(1);
-    }
+    imu.resetMPU9250(); // start by resetting MPU9250
 
     // Read the WHO_AM_I register, this is a good test of communication
     uint8_t c = imu.getMPU9250ID();
@@ -36,8 +38,6 @@ int main(int argc, char ** argv)
         fprintf(stderr, "Could not connect to MPU9250: 0x%02x\n", c);
         exit(1);
     }
-
-    imu.resetMPU9250(); // start by resetting MPU9250
 
     // Read the WHO_AM_I register of the magnetometer, this is a good test of communication
     uint8_t d = imu.getAK8963CID();  // Read WHO_AM_I register for AK8963
