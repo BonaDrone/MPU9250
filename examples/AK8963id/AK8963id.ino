d9abb3713b7bb8e8b578668b1625ab30834c2a00
simondlevy <simon.d.levy@gmail.com>
2018-04-29 00:47:30 -0400

    Cleanup

--- examples/AK8963id/AK8963id.ino
+++ examples/AK8963id/AK8963id.ino
@@ -1,5 +1,13 @@
 #include <Wire.h>
 
+// Define I2C addresses of MPU9250
+#define ADO 0
+#if ADO
+static const uint8_t MPU9250_ADDRESS  = 0x69;   // Device address when ADO = 1
+#else
+static const uint8_t MPU9250_ADDRESS  = 0x68;  // Device address when ADO = 0
+#endif  
+
 // MPU9250 registers
 const uint8_t EXT_SENS_DATA_00 = 0x49;
 
@@ -29,19 +37,19 @@ const uint8_t AK8963_RESET = 0x01;
 
 const uint8_t AK8963_WHO_AM_I = 0x00;
 
-static void readRegisters(uint8_t subAddress, uint8_t count, uint8_t* dest){
-    Wire.beginTransmission(0x68); // open the device
+static void readRegisters(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t* dest){
+    Wire.beginTransmission(address); // open the device
     Wire.write(subAddress); // specify the starting register address
     Wire.endTransmission(false);
-    Wire.requestFrom(0x68, count); // specify the number of bytes to receive
+    Wire.requestFrom(address, count); // specify the number of bytes to receive
     for(uint8_t i = 0; i < count; i++){ 
         dest[i] = Wire.read();
     }
 }
 
-static void writeRegister(uint8_t subAddress, uint8_t data)
+static void writeRegister(uint8_t address, uint8_t subAddress, uint8_t data)
 {
-    Wire.beginTransmission(0x68); // open the device
+    Wire.beginTransmission(address); // open the device
     Wire.write(subAddress); // write the register address
     Wire.write(data); // write the data
     Wire.endTransmission();
@@ -51,34 +59,34 @@ static void writeRegister(uint8_t subAddress, uint8_t data)
 static void readAK8963Registers(uint8_t subAddress, uint8_t count, uint8_t* dest)
 {
     // set slave 0 to the AK8963 and set for read
-    writeRegister(I2C_SLV0_ADDR,AK8963_I2C_ADDR | I2C_READ_FLAG);
+    writeRegister(MPU9250_ADDRESS, I2C_SLV0_ADDR,AK8963_I2C_ADDR | I2C_READ_FLAG);
 
     // set the register to the desired AK8963 sub address
-    writeRegister(I2C_SLV0_REG,subAddress);
+    writeRegister(MPU9250_ADDRESS, I2C_SLV0_REG,subAddress);
 
     // enable I2C and request the bytes
-    writeRegister(I2C_SLV0_CTRL,I2C_SLV0_EN | count);
+    writeRegister(MPU9250_ADDRESS, I2C_SLV0_CTRL,I2C_SLV0_EN | count);
 
     delay(1); // takes some time for these registers to fill
 
     // read the bytes off the MPU9250 EXT_SENS_DATA registers
-    readRegisters(EXT_SENS_DATA_00,count,dest); 
+    readRegisters(MPU9250_ADDRESS, EXT_SENS_DATA_00,count,dest); 
 }
 
 /* writes a register to the AK8963 given a register address and data */
 static void writeAK8963Register(uint8_t subAddress, uint8_t data)
 {
     // set slave 0 to the AK8963 and set for write
-    writeRegister(I2C_SLV0_ADDR,AK8963_I2C_ADDR);
+    writeRegister(MPU9250_ADDRESS, I2C_SLV0_ADDR,AK8963_I2C_ADDR);
 
     // set the register to the desired AK8963 sub address 
-    writeRegister(I2C_SLV0_REG,subAddress);
+    writeRegister(MPU9250_ADDRESS, I2C_SLV0_REG,subAddress);
 
     // store the data for write
-    writeRegister(I2C_SLV0_DO,data);
+    writeRegister(MPU9250_ADDRESS, I2C_SLV0_DO,data);
 
     // enable I2C and send 1 byte
-    writeRegister(I2C_SLV0_CTRL,I2C_SLV0_EN | (uint8_t)1);
+    writeRegister(MPU9250_ADDRESS, I2C_SLV0_CTRL,I2C_SLV0_EN | (uint8_t)1);
 
     // pause briefly to complete writing
     delay(1);
@@ -104,19 +112,19 @@ void setup(void)
     Wire.setClock(400000);
 
     // select clock source to gyro
-    writeRegister(PWR_MGMNT_1,CLOCK_SEL_PLL);
+    writeRegister(MPU9250_ADDRESS, PWR_MGMNT_1,CLOCK_SEL_PLL);
 
     // enable I2C master mode
-    writeRegister(USER_CTRL,I2C_MST_EN);
+    writeRegister(MPU9250_ADDRESS, USER_CTRL,I2C_MST_EN);
 
     // set the I2C bus speed to 400 kHz
-    writeRegister(I2C_MST_CTRL,I2C_MST_CLK);
+    writeRegister(MPU9250_ADDRESS, I2C_MST_CTRL,I2C_MST_CLK);
 
     // set AK8963 to Power Down
     writeAK8963Register(AK8963_CNTL1,AK8963_PWR_DOWN);
 
     // reset the MPU9250
-    writeRegister(PWR_MGMNT_1,PWR_RESET);
+    writeRegister(MPU9250_ADDRESS, PWR_MGMNT_1,PWR_RESET);
 
     // wait for MPU-9250 to come back up
     delay(1);
@@ -125,13 +133,13 @@ void setup(void)
     writeAK8963Register(AK8963_CNTL2,AK8963_RESET);
 
     // select clock source to gyro
-    writeRegister(PWR_MGMNT_1,CLOCK_SEL_PLL);
+    writeRegister(MPU9250_ADDRESS, PWR_MGMNT_1,CLOCK_SEL_PLL);
 
     // enable I2C master mode
-    writeRegister(USER_CTRL,I2C_MST_EN);
+    writeRegister(MPU9250_ADDRESS, USER_CTRL,I2C_MST_EN);
 
     // set the I2C bus speed to 400 kHz
-    writeRegister(I2C_MST_CTRL,I2C_MST_CLK);
+    writeRegister(MPU9250_ADDRESS, I2C_MST_CTRL,I2C_MST_CLK);
 
     // check AK8963 WHO AM I register, expected value is 0x48 (decimal 72)
     uint8_t addr = whoAmIAK8963();
