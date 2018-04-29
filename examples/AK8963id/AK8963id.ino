b83a6fbffaf0c1757b21c6fc8735e3f0b7d50f24
simondlevy <simon.d.levy@gmail.com>
2018-04-28 23:13:49 -0400

    Cleanup

--- examples/AK8963id/AK8963id.ino
+++ examples/AK8963id/AK8963id.ino
@@ -17,47 +17,28 @@ const uint8_t I2C_READ_FLAG = 0x80;
 const uint8_t AK8963_I2C_ADDR = 0x0C;
 const uint8_t AK8963_WHO_AM_I = 0x00;
 
-static uint8_t _buffer[21];
-
-static void readRegisters(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t* dest)
-{
-    Wire.beginTransmission(address); // open the device
-    Wire.write(subAddress); // specify the starting register address
-    Wire.endTransmission(false);
-    Wire.requestFrom(0x68, count); // specify the number of bytes to receive
-    for(uint8_t i = 0; i < count; i++){ 
-        dest[i] = Wire.read();
-    }
-}
-
-static void writeRegister(uint8_t address, uint8_t subAddress, uint8_t data)
-{
-    Wire.beginTransmission(address); // open the device
-    Wire.write(subAddress); // write the register address
-    Wire.write(data); // write the data
-    Wire.endTransmission();
-}
-
 /* reads registers from the AK8963 */
 static void readAK8963Registers(uint8_t subAddress, uint8_t count, uint8_t* dest)
 {
     // set slave 0 to the AK8963 and set for read
-    writeRegister(0x68, I2C_SLV0_ADDR,AK8963_I2C_ADDR | I2C_READ_FLAG);
+    bt.writeRegister(0x68, I2C_SLV0_ADDR,AK8963_I2C_ADDR | I2C_READ_FLAG);
 
     // set the register to the desired AK8963 sub address
-    writeRegister(0x68, I2C_SLV0_REG,subAddress);
+    bt.writeRegister(0x68, I2C_SLV0_REG,subAddress);
 
     // enable I2C and request the bytes
-    writeRegister(0x68, I2C_SLV0_CTRL,I2C_SLV0_EN | count);
+    bt.writeRegister(0x68, I2C_SLV0_CTRL,I2C_SLV0_EN | count);
 
     delay(1); // takes some time for these registers to fill
 
     // read the bytes off the MPU9250 EXT_SENS_DATA registers
-    readRegisters(0x68, EXT_SENS_DATA_00, count,dest); 
+    bt.readRegisters(0x68, EXT_SENS_DATA_00, count,dest); 
 }
 
 static uint8_t whoAmIAK8963()
 {
+    static uint8_t _buffer[21];
+
     // read the WHO AM I register
     readAK8963Registers(AK8963_WHO_AM_I,1,_buffer);
 
