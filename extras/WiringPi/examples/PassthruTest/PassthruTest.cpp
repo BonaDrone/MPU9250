c5f38ed27aab054e15f7dee1d9a31da47be9335a
simondlevy <simon.d.levy@gmail.com>
2018-05-06 02:48:36 +0000

    Begin pass-through example

--- extras/WiringPi/examples/PassthruTest/PassthruTest.cpp
+++ extras/WiringPi/examples/PassthruTest/PassthruTest.cpp
@@ -4,7 +4,7 @@
  *
  * Adapted for WiringPi by Simon D. Levy April 2018
  *  
- * Demonstrate basic MPU-9250 functionality in pass-through mode including
+ * Demonstrate basic MPU-9250 functionality in master mode including
  * parameterizing the register addresses, initializing the sensor, getting
  * properly scaled accelerometer, gyroscope, and magnetometer data out. 
  *
@@ -16,6 +16,8 @@
 #include <stdio.h>
 
 #include <MPU9250.h>
+
+#include <wiringPi.h>
 #include <WiringPiTransfer.h>
 
 /*
@@ -43,15 +45,14 @@ static const uint8_t sampleRate = 0x04;
 static float aRes, gRes, mRes;
 
 // Pin definitions
-//static const uint8_t intPin = 8;   //  MPU9250 interrupt
-//static const uint8_t ledPin = 13; // red led
+static const uint8_t intPin = 0;   //  MPU9250 interrupt
 
 // Interrupt support 
-//static bool gotNewData = false;
-//static void myinthandler()
-//{
-//    gotNewData = true;
-//}
+static bool gotNewData;
+static void myinthandler()
+{
+    gotNewData = true;
+}
 
 // Factory mag calibration and mag bias
 static float   magCalibration[3]; 
@@ -62,40 +63,30 @@ static float gyroBias[3], accelBias[3], magBias[3]={0,0,0}, magScale[3]={1,1,1};
 
 // Create a byte-transfer object for WiringPi I^2C
 WiringPiI2C mpu(MPU9250::MPU9250_ADDRESS);
-WiringPiI2C mag(MPU9250::AK8963_ADDRESS);
 
-// Instantiate MPU9250 class in pass-through mode
-static MPU9250Passthru imu = MPU9250Passthru(&mpu, &mag); 
+// Instantiate MPU9250 class in master mode
+static MPU9250Master imu = MPU9250Master(&mpu); 
 
 static void setup()
 {
-    /*
+    // Setup WirinPi
+    wiringPiSetup();
 
-    Wire.begin(); 
-    Wire.setClock(400000); 
-    delay(1000);
+    // Start I^2 on the MPU9250 address
+    mpu.begin();
 
-    // Set up the interrupt pin, it's set as active high, push-pull
-    pinMode(intPin, INPUT);
-
-    // Start with orange led on (active HIGH)
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, HIGH); 
+    delay(100);
 
     // Configure the MPU9250 
     // Read the WHO_AM_I register, this is a good test of communication
-    Serial.println("MPU9250 9-axis motion sensor...");
+    printf("MPU9250 9-axis motion sensor...\n");
     uint8_t c = imu.getMPU9250ID();
-    Serial.print("MPU9250 ");
-    Serial.print("I AM ");
-    Serial.print(c, HEX);
-    Serial.print(" I should be ");
-    Serial.println(0x71, HEX);
+    printf("MPU9250  I AM %02X  I should be 0x71\n", c);
     delay(1000);
 
     if (c == 0x71 ) { // WHO_AM_I should always be 0x71 for MPU9250, 0x73 for MPU9255 
     
-        Serial.println("MPU9250 is online...");
+        printf("MPU9250 is online...\n");
 
         imu.resetMPU9250(); // start by resetting MPU9250
 
@@ -103,24 +94,12 @@ static void setup()
 
         imu.SelfTest(SelfTest); // Start by performing self test and reporting values
 
-        Serial.print("x-axis self test: acceleration trim within : "); 
-        Serial.print(SelfTest[0],1); 
-        Serial.println("% of factory value");
-        Serial.print("y-axis self test: acceleration trim within : "); 
-        Serial.print(SelfTest[1],1); 
-        Serial.println("% of factory value");
-        Serial.print("z-axis self test: acceleration trim within : "); 
-        Serial.print(SelfTest[2],1); 
-        Serial.println("% of factory value");
-        Serial.print("x-axis self test: gyration trim within : "); 
-        Serial.print(SelfTest[3],1); 
-        Serial.println("% of factory value");
-        Serial.print("y-axis self test: gyration trim within : "); 
-        Serial.print(SelfTest[4],1); 
-        Serial.println("% of factory value");
-        Serial.print("z-axis self test: gyration trim within : "); 
-        Serial.print(SelfTest[5],1); 
-        Serial.println("% of factory value");
+        printf("x-axis self test: acceleration trim within : %+3.3f%% of factory value\n", SelfTest[0]); 
+        printf("y-axis self test: acceleration trim within : %+3.3f%% of factory value\n", SelfTest[1]); 
+        printf("z-axis self test: acceleration trim within : %+3.3f%% of factory value\n", SelfTest[2]); 
+        printf("x-axis self test: gyration trim within : %+3.3f%% of factory value\n", SelfTest[3]); 
+        printf("y-axis self test: gyration trim within : %+3.3f%% of factory value\n", SelfTest[4]); 
+        printf("z-axis self test: gyration trim within : %+3.3f%% of factory value\n", SelfTest[5]); 
         delay(1000);
 
         // get sensor resolutions, only need to do this once
@@ -130,73 +109,62 @@ static void setup()
 
         // Comment out if using pre-measured, pre-stored offset accel/gyro biases
         imu.calibrateMPU9250(gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
-        Serial.println("accel biases (mg)");
-        Serial.println(1000.*accelBias[0]);
-        Serial.println(1000.*accelBias[1]);
-        Serial.println(1000.*accelBias[2]);
-        Serial.println("gyro biases (dps)");
-        Serial.println(gyroBias[0]);
-        Serial.println(gyroBias[1]);
-        Serial.println(gyroBias[2]);
+        printf("accel biases (mg)\n");
+        printf("%f\n", 1000.*accelBias[0]);
+        printf("%f\n", 1000.*accelBias[1]);
+        printf("%f\n", 1000.*accelBias[2]);
+        printf("gyro biases (dps)\n");
+        printf("%f\n", gyroBias[0]);
+        printf("%f\n", gyroBias[1]);
+        printf("%f\n", gyroBias[2]);
         delay(1000); 
 
         imu.initMPU9250(Ascale, Gscale, sampleRate); 
-        Serial.println("MPU9250 initialized for active data mode...."); 
+        printf("MPU9250 initialized for active data mode....\n"); 
 
         // Read the WHO_AM_I register of the magnetometer, this is a good test of communication
-        byte d = imu.getAK8963CID();  // Read WHO_AM_I register for AK8963
-        Serial.print("AK8963 ");
-        Serial.print("I AM ");
-        Serial.print(d, HEX);
-        Serial.print(" I should be ");
-        Serial.println(0x48, HEX);
+        uint8_t d = imu.getAK8963CID();  // Read WHO_AM_I register for AK8963
+        printf("AK8963  I AM 0x%02x  I should be 0x48\n", d);
         delay(1000); 
 
         // Get magnetometer calibration from AK8963 ROM
         imu.initAK8963(Mscale, Mmode, magCalibration);
-        Serial.println("AK8963 initialized for active data mode...."); 
+        printf("AK8963 initialized for active data mode....\n"); 
 
         // Comment out if using pre-measured, pre-stored offset magnetometer biases
-        Serial.println("Mag Calibration: Wave device in a figure eight until done!");
+        /*
+        printf("Mag Calibration: Wave device in a figure eight until done!\n");
         delay(4000);
         imu.magcalMPU9250(magBias, magScale);
-        Serial.println("Mag Calibration done!");
-        Serial.println("AK8963 mag biases (mG)");
-        Serial.println(magBias[0]);
-        Serial.println(magBias[1]);
-        Serial.println(magBias[2]); 
-        Serial.println("AK8963 mag scale (mG)");
-        Serial.println(magScale[0]);
-        Serial.println(magScale[1]);
-        Serial.println(magScale[2]); 
+        printf("Mag Calibration done!\n");
+        printf("AK8963 mag biases (mG)\n");
+        printf("%f\n", magBias[0]);
+        printf("%f\n", magBias[1]);
+        printf("%f\n", magBias[2]); 
+        printf("AK8963 mag scale (mG)\n");
+        printf("%f\n", magScale[0]);
+        printf("%f\n", magScale[1]);
+        printf("%f\n", magScale[2]); 
         delay(2000); // add delay to see results before serial spew of data
-        Serial.println("Calibration values: ");
-        Serial.print("X-Axis sensitivity adjustment value ");
-        Serial.println(magCalibration[0], 2);
-        Serial.print("Y-Axis sensitivity adjustment value ");
-        Serial.println(magCalibration[1], 2);
-        Serial.print("Z-Axis sensitivity adjustment value ");
-        Serial.println(magCalibration[2], 2);
-
-        attachInterrupt(intPin, myinthandler, RISING);  // define interrupt for intPin output of MPU9250
-
+        printf("Calibration values:\n");
+        printf("X-Axis sensitivity adjustment value %+2.2f\n", magCalibration[0]);
+        printf("Y-Axis sensitivity adjustment value %+2.2f\n", magCalibration[1]);
+        printf("Z-Axis sensitivity adjustment value %+2.2f\n", magCalibration[2]);
+        */
+        
+        wiringPiISR(intPin, INT_EDGE_RISING, &myinthandler);// define interrupt for intPin output of MPU9250
     }
     else {
 
-        Serial.print("Could not connect to MPU9250: 0x");
-        Serial.println(c, HEX);
-        while(1) ; // Loop forever if communication doesn't happen
+        printf("Could not connect to MPU9250: 0x%02x", c);
+        while(true) ; // Loop forever if communication doesn't happen
     }
 
-    digitalWrite(ledPin, LOW); // turn off led when using flash memory
-
     delay(3000);                // wait a bit before looping
-    */
 }
 
 static void loop()
 {  
-    /*
     static int16_t MPU9250Data[7]; // used to read all 14 bytes at once from the MPU9250 accel/gyro
     static float ax, ay, az, gx, gy, gz, mx, my, mz;
 
@@ -204,9 +172,9 @@ static void loop()
     // or the accel wake on motion threshold has been crossed
     if(gotNewData) {   // On interrupt, read data
 
-        gotNewData = false;     // reset gotNewData flag
+       gotNewData = false;     // reset gotNewData flag
 
-        if (imu.checkNewAccelGyroData())  { // data ready interrupt is detected
+        if (imu.checkNewData())  { // data ready interrupt is detected
 
             imu.readMPU9250Data(MPU9250Data); // INT cleared on any read
 
@@ -220,22 +188,19 @@ static void loop()
             gy = (float)MPU9250Data[5]*gRes;  
             gz = (float)MPU9250Data[6]*gRes; 
 
-            if(imu.checkNewMagData()) { // wait for magnetometer data ready bit to be set
+            int16_t magCount[3];    // Stores the 16-bit signed magnetometer sensor output
 
-                int16_t magCount[3];    // Stores the 16-bit signed magnetometer sensor output
+            imu.readMagData(magCount);  // Read the x/y/z adc values
 
-                imu.readMagData(magCount);  // Read the x/y/z adc values
-
-                // Calculate the magnetometer values in milliGauss
-                // Include factory calibration per data sheet and user environmental corrections
-                // Get actual magnetometer value, this depends on scale being set
-                mx = (float)magCount[0]*mRes*magCalibration[0] - magBias[0];  
-                my = (float)magCount[1]*mRes*magCalibration[1] - magBias[1];  
-                mz = (float)magCount[2]*mRes*magCalibration[2] - magBias[2];  
-                mx *= magScale[0];
-                my *= magScale[1];
-                mz *= magScale[2]; 
-            }
+            // Calculate the magnetometer values in milliGauss
+            // Include factory calibration per data sheet and user environmental corrections
+            // Get actual magnetometer value, this depends on scale being set
+            mx = (float)magCount[0]*mRes*magCalibration[0] - magBias[0];  
+            my = (float)magCount[1]*mRes*magCalibration[1] - magBias[1];  
+            mz = (float)magCount[2]*mRes*magCalibration[2] - magBias[2];  
+            mx *= magScale[0];
+            my *= magScale[1];
+            mz *= magScale[2]; 
         }
 
         // Report at 1Hz
@@ -246,40 +211,17 @@ static void loop()
 
             msec_prev = msec_curr;
 
-            Serial.print("ax = ");
-            Serial.print((int)1000*ax);  
-            Serial.print(" ay = ");
-            Serial.print((int)1000*ay); 
-            Serial.print(" az = ");
-            Serial.print((int)1000*az);
-            Serial.println(" mg");
-            Serial.print("gx = ");
-            Serial.print( gx, 2); 
-            Serial.print(" gy = ");
-            Serial.print( gy, 2); 
-            Serial.print(" gz = ");
-            Serial.print( gz, 2);
-            Serial.println(" deg/s");
-            Serial.print("mx = ");
-            Serial.print( (int)mx ); 
-            Serial.print(" my = ");
-            Serial.print( (int)my ); 
-            Serial.print(" mz = ");
-            Serial.print( (int)mz );
-            Serial.println(" mG");
+            printf("ax = %d  ay = %d  az = %d mg\n", (int)(1000*ax), (int)(1000*ay), (int)(1000*az));
+            printf("gx = %+2.2f  gy = %+2.2f  gz = %+2.2f deg/s\n", gx, gy, gz);
+            printf("mx = %d  my = %d  mz = %d mG\n", (int)mx, (int)my, (int)mz);
 
             float temperature = ((float) MPU9250Data[3]) / 333.87f + 21.0f; // Gyro chip temperature in degrees Centigrade
 
             // Print temperature in degrees Centigrade      
-            Serial.print("Gyro temperature is ");  
-            Serial.print(temperature, 1);  
-            Serial.println(" degrees C"); 
-
+            printf("Gyro temperature is %+1.1f degrees C\n", temperature);  
         }
 
     } // if got new data
-
-    */
 }
 
 int main(int argc, char ** argv)
