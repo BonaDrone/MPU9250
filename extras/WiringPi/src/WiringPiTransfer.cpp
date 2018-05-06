b195ffc516c5ff95a120d3bfa1834027363aa1e2
simondlevy <simon.d.levy@gmail.com>
2018-05-06 01:55:26 +0000

    Successfully retrieved WhoAmI register from MPU9250

--- extras/WiringPi/src/WiringPiTransfer.cpp
+++ extras/WiringPi/src/WiringPiTransfer.cpp
@@ -10,6 +10,9 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 void WiringPiI2C::begin(void)
 {
     _fd = wiringPiI2CSetup (_address);
@@ -17,17 +20,21 @@ void WiringPiI2C::begin(void)
 
 void WiringPiI2C::writeRegister(uint8_t subAddress, uint8_t data)
 {
+	wiringPiI2CWriteReg8(_fd, subAddress, data);
 }
 
 uint8_t WiringPiI2C::readRegister(uint8_t subAddress)
 {
     uint8_t data = 0;
-
-    return data;                             // Return data read from slave register
+    readRegisters(subAddress, 1, &data);
+    return data;                             
 }
 
 void WiringPiI2C::readRegisters(uint8_t subAddress, uint8_t count, uint8_t * dest)
 {  
+    for (uint8_t i=0; i<count; ++i) {
+        dest[i] = wiringPiI2CReadReg8(_fd, subAddress+i);
+    }
 }
 
 
