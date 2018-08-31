177589cef18fac0493f9b2812d9d3b7a5347de5f
simondlevy <simon.d.levy@gmail.com>
2018-08-31 06:13:09 +0000

    Restructured extras/code; compiling on TX1

--- extras/code/i2cdev/timing.cpp
+++ extras/code/i2cdev/timing.cpp
@@ -28,6 +28,13 @@ void delay(unsigned int msec)
     usleep(msec*1000);
 }
 
+unsigned int millis(void)
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return 1000*ts.tv_sec + ts.tv_nsec/1000000;
+}
+
 unsigned int micros(void)
 {
     struct timespec ts;
